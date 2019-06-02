3064849c13981864c4d85667659d502f0c3b5d9f
William A. Kennington III <wak@google.com>
2019-06-02 02:05:03 -0700

    future

--- test/utility/future.cpp
+++ test/utility/future.cpp
@@ -1,9 +1,9 @@
 #include <chrono>
-#include <future>
 #include <gtest/gtest.h>
 #include <sdeventplus/event.hpp>
 #include <sdeventplus/source/event.hpp>
 #include <sdeventplus/utility/future.hpp>
+#include <stdplus/util/future.hpp>
 #include <utility>
 
 namespace sdeventplus
@@ -46,9 +46,9 @@ TEST_F(CallWhenReady, NoFutures)
 
 TEST_F(CallWhenReady, BasicFuture)
 {
-    std::promise<int> p;
+    stdplus::util::Promise<int> p;
     size_t cbCalls = 0;
-    auto cb = [&](const Event&, std::future<int>&& f) {
+    auto cb = [&](const Event&, stdplus::util::Future<int>&& f) {
         f.get(); // Ensure the future is ready since this will block otherwise
         cbCalls++;
     };
@@ -59,11 +59,18 @@ TEST_F(CallWhenReady, BasicFuture)
     {
         EXPECT_EQ(1, event.dispatch());
     }
-    p.set_value(10);
-    // Ensure the promise setting didn't trigger new events to run
     EXPECT_EQ(0, event.wait(0s));
 
-    // Add a new event and and ensure the promise is fullfilled
+    // Ensure the promise setting triggered new events to run
+    p.set_value(10);
+    while (event.prepare() > 0)
+    {
+        EXPECT_EQ(1, event.dispatch());
+    }
+    EXPECT_EQ(1, cbCalls);
+    EXPECT_EQ(0, event.wait(0s));
+
+    // Ensure we are called only once
     source::Defer(event, [](source::EventBase&) {}).set_floating(true);
     while (event.prepare() > 0)
     {
@@ -75,9 +82,10 @@ TEST_F(CallWhenReady, BasicFuture)
 
 TEST_F(CallWhenReady, TwoFutures)
 {
-    std::promise<int> p1, p2;
+    stdplus::util::Promise<int> p1, p2;
     size_t cbCalls = 0;
-    auto cb = [&](const Event&, std::future<int>&& f1, std::future<int>&& f2) {
+    auto cb = [&](const Event&, stdplus::util::Future<int>&& f1,
+                  stdplus::util::Future<int>&& f2) {
         f1.get();
         f2.get();
         cbCalls++;
@@ -92,8 +100,10 @@ TEST_F(CallWhenReady, TwoFutures)
     }
     EXPECT_EQ(0, event.wait(0s));
 
-    // Add a new event and only fullfill a single promise
+    // Only fullfill a single promise
     p1.set_value(10);
+
+    // Add a new event and ensure we aren't executed
     source::Defer(event, [](source::EventBase&) {}).set_floating(true);
     while (event.prepare() > 0)
     {
@@ -102,8 +112,16 @@ TEST_F(CallWhenReady, TwoFutures)
     EXPECT_EQ(0, cbCalls);
     EXPECT_EQ(0, event.wait(0s));
 
-    // Add a new event and fullfill all promises
+    // Fullfill all promises and ensure the loop is triggered
     p2.set_value(20);
+    while (event.prepare() > 0)
+    {
+        EXPECT_EQ(1, event.dispatch());
+    }
+    EXPECT_EQ(1, cbCalls);
+    EXPECT_EQ(0, event.wait(0s));
+
+    // Ensure we are called only once
     source::Defer(event, [](source::EventBase&) {}).set_floating(true);
     while (event.prepare() > 0)
     {
@@ -111,6 +129,31 @@ TEST_F(CallWhenReady, TwoFutures)
     }
     EXPECT_EQ(1, cbCalls);
     EXPECT_EQ(0, event.wait(0s));
+}
+
+TEST_F(CallWhenReady, VariableSize)
+{
+    std::vector<stdplus::util::Future<int>> v;
+    for (int i = 0; i < 5; ++i)
+    {
+        stdplus::util::Promise<int> p;
+        v.push_back(p.get_future());
+        p.set_value(i);
+    }
+    int val = 0;
+    auto cb = [&](const Event&, std::vector<stdplus::util::Future<int>>&& vs) {
+        for (const auto& v : vs)
+        {
+            val += v.get();
+        }
+    };
+    callWhenReady(event, std::move(cb), std::move(v)).set_floating(true);
+    while (event.prepare() > 0)
+    {
+        EXPECT_EQ(1, event.dispatch());
+    }
+    EXPECT_EQ(0, event.wait(0s));
+    EXPECT_EQ(10, val);
 }
 
 } // namespace
